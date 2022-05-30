66f76a032a39940a5f83b8d575cc72710724279d
amig05c <amitghosh0595c@gmail.com>
2022-05-30 16:47:12 +0530

    evaluation 2

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
-const Product = () => {
+const Product = ({products}) => {
   // TODO: Remove below const and instead import them from chakra
+  // console.log(products[0].title)
+  
   const Text = () => <div />;
   const Image = () => <div />;
   const Box = () => <div />;
@@ -10,15 +12,30 @@ const Product = () => {
   const Tag = () => <div />;
   const TagLabel = () => <div />;
   return (
-    <Stack data-cy="product">
-      <Image data-cy="product-image" />
-      <Text data-cy="product-category"></Text>
-      <Tag>
-        <TagLabel data-cy="product-gender"></TagLabel>
-      </Tag>
-      <Heading data-cy="product-title"></Heading>
-      <Box data-cy="product-price"></Box>
-    </Stack>
+    <div>
+      {/* <h1>Amit</h1> */}
+      {products.map(elem=>(
+        // <h1>{console.log(elem)}</h1>
+        //   <Stack key={elem.id} data-cy="product">
+        //   <Image data-cy="product-image" src={elem.imageSrc} />
+        //   <Text data-cy="product-category"><p>{elem.title}</p></Text>
+        //   <Tag>
+        //     <TagLabel data-cy="product-gender">{elem.gender}</TagLabel>
+        //   </Tag>
+        //   <Heading data-cy="product-title">{elem.title}</Heading>
+        //   <Box data-cy="product-price">{elem.price}</Box>
+        // </Stack>
+        <div key={elem.id} data-cy="product">
+          <p data-cy="product-title">{elem.title}</p>
+          <img data-cy="product-image" src={elem.imageSrc} alt="" />
+          <p data-cy="product-gender">{elem.gender}</p>
+          <p data-cy="product-category">{elem.catagory}</p>
+          <p data-cy="product-price">{elem.price}</p>
+        </div>
+        
+      ))}
+    
+    </div>
   );
 };
 
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,16 +1,42 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
+import AddProduct from "./AddProduct"
+import Product from "./Product"
+import Pagination from "./Pagination"
+import axios from "axios"
+
 
 const Products = () => {
   // TODO: Remove below const and instead import them from chakra
-  const Flex = () => <div />;
-  const Grid = () => <div />;
+  const [products,setProducts]= useState([])
+  // const Flex = () => <div />;
+  // const Grid = () => <div />;
+
+useEffect(()=>{
+  const getdata = async () => {
+    let result = await axios.get(
+      `http://localhost:8080/products`);
+      // console.log(result.data)
+      setProducts(result.data)
+    }
+    getdata()
+},[])
+
+  
+
+
 
   return (
-    <Flex>
-      {/*  AddProduct */}
-      <Grid>{/* List of Products */}</Grid>
-      {/* Pagination */}
-    </Flex>
+    // <Flex>
+    <div>
+       <AddProduct/>
+        
+      {/* // <Grid> */}
+        {/* {console.log(products)} */}
+      <Product products={products}/>
+      {/* </Grid> */}
+       <Pagination/>
+    {/* // </Flex> */}
+    </div>
   );
 };
 
